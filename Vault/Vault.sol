feb98a27b5e232fd79c7602ac84ea33dd57b443c
lopeselio <lopeselio@gmail.com>
2023-01-26 16:37:35 +0530

    fetched password bytes32 & unlock vault

--- Vault/Vault.sol
+++ Vault/Vault.sol
@@ -1,1 +1,20 @@
+// 0x412076657279207374726f6e67207365637265742070617373776f7264203a29
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Vault {
+  bool public locked;
+  bytes32 private password;
+
+  constructor(bytes32 _password) {
+    locked = true;
+    password = _password;
+  }
+
+  function unlock(bytes32 _password) public {
+    if (password == _password) {
+      locked = false;
+    }
+  }
+}
